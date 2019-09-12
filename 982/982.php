21d6026af56aaf0483523402e42d85df5b6d87d2
foostart.com <foostart.com@gmail.com>
2019-09-12 09:46:46 +0700

    cm

---
+++ 982/982.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>jquery sliders</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css" type="text/css" media="screen" />
+    <script type="text/javascript">
+        var _siteRoot = 'tdc_mod_custom_17_B30.html',
+            _root = 'tdc_mod_custom_17_B30.html';
+    </script>
+    <script type="text/javascript" src="js/jquery.js"></script>
+    <script type="text/javascript" src="js/scripts.js"></script>
+    <script src="js/ie6.js" type="text/javascript"></script>
+</head>
+<body>
+    <!--/top-->
+    <div id="header">
+        <div class="wrap">
+            <div id="slide-holder">
+                <div id="slide-runner">
+                    <a href=""><img id="slide-img-1" src="images/1.png" class="slide" alt="" /></a>
+                    <a href=""><img id="slide-img-2" src="images/2.png" class="slide" alt="" /></a>
+                    <a href=""><img id="slide-img-3" src="images/3.png" class="slide" alt="" /></a>
+                    <a href=""><img id="slide-img-4" src="images/4.png" class="slide" alt="" /></a>
+                    <a href=""><img id="slide-img-5" src="images/5.png" class="slide" alt="" /></a>
+                    <a href=""><img id="slide-img-6" src="images/6.png" class="slide" alt="" /></a>
+                    <a href=""><img id="slide-img-7" src="images/7.png" class="slide" alt="" /></a>
+                    <div id="slide-controls">
+                        <p id="slide-client" class="text"><strong>post: </strong><span></span></p>
+                        <p id="slide-desc" class="text"></p>
+                        <p id="slide-nav"></p>
+                    </div>
+                </div>
+                <!--content featured gallery here -->
+            </div>
+            <script type="text/javascript">
+                if (!window.slider)
+                    var slider = {};
+                slider.data = [{
+                    "id": "slide-img-1",
+                    "client": "nature beauty",
+                    "desc": "nature beauty photography"
+                }, {
+                    "id": "slide-img-2",
+                    "client": "nature beauty",
+                    "desc": "add your description here"
+                }, {
+                    "id": "slide-img-3",
+                    "client": "nature beauty",
+                    "desc": "add your description here"
+                }, {
+                    "id": "slide-img-4",
+                    "client": "nature beauty",
+                    "desc": "add your description here"
+                }, {
+                    "id": "slide-img-5",
+                    "client": "nature beauty",
+                    "desc": "add your description here"
+                }, {
+                    "id": "slide-img-6",
+                    "client": "nature beauty",
+                    "desc": "add your description here"
+                }, {
+                    "id": "slide-img-7",
+                    "client": "nature beauty",
+                    "desc": "add your description here"
+                }];
+            </script>
+        </div>
+    </div>
+    <!--/header-->
+</body>
+</html>
