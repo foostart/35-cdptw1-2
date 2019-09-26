8a8c02b20ec311748b2d09f31e7920da06e058c0
tdc-students <cuongtk@tdc.edu.vn>
2019-09-26 11:02:58 +0700

    edit 7
    edit 7

--- 982/982.php
+++ 982/982.php
@@ -4,72 +4,28 @@
     <title>892 sliders</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css" type="text/css" media="screen" />
-    <script>
-        var _siteRoot = 'tdc_mod_custom_17_B30.html',
-            _root = 'tdc_mod_custom_17_B30.html';
+    <link rel="stylesheet" href="https://unpkg.com/swiper/css/swiper.min.css">
+   
+</head>
+<body>
+    <?php include '/982-content.php'; ?>
+    <!--/top-->
+    
+    <!--/header-->
+</body>
+</html>
+ <script>
+       
     </script>
     <script src="js/jquery.js"></script>
     <script src="js/scripts.js"></script>
     <script src="js/ie6.js" ></script>
-</head>
-<body>
-    <!--/top-->
-    <div id="header">
-        <div class="wrap">
-            <div id="slide-holder">
-                <div id="slide-runner">
-                    <a href=""><img id="slide-img-1" src="images/1.png" class="slide" alt="" /></a>
-                    <a href=""><img id="slide-img-2" src="images/2.png" class="slide" alt="" /></a>
-                    <a href=""><img id="slide-img-3" src="images/3.png" class="slide" alt="" /></a>
-                    <a href=""><img id="slide-img-4" src="images/4.png" class="slide" alt="" /></a>
-                    <a href=""><img id="slide-img-5" src="images/5.png" class="slide" alt="" /></a>
-                    <a href=""><img id="slide-img-6" src="images/6.png" class="slide" alt="" /></a>
-                    <a href=""><img id="slide-img-7" src="images/7.png" class="slide" alt="" /></a>
-                    <div id="slide-controls">
-                        <p id="slide-client" class="text"><strong>post: </strong><span></span></p>
-                        <p id="slide-desc" class="text"></p>
-                        <p id="slide-nav">
-                            
-                        </p>
-                    </div>
-                </div>
-                <!--content featured gallery here -->
-            </div>
-            <script>
-                if (!window.slider)
-                    var slider = {};
-                slider.data = [{
-                    "id": "slide-img-1",
-                    "client": "nature beauty",
-                    "desc": "nature beauty photography"
-                }, {
-                    "id": "slide-img-2",
-                    "client": "nature beauty",
-                    "desc": "add your description here"
-                }, {
-                    "id": "slide-img-3",
-                    "client": "nature beauty",
-                    "desc": "add your description here"
-                }, {
-                    "id": "slide-img-4",
-                    "client": "nature beauty",
-                    "desc": "add your description here"
-                }, {
-                    "id": "slide-img-5",
-                    "client": "nature beauty",
-                    "desc": "add your description here"
-                }, {
-                    "id": "slide-img-6",
-                    "client": "nature beauty",
-                    "desc": "add your description here"
-                }, {
-                    "id": "slide-img-7",
-                    "client": "nature beauty",
-                    "desc": "add your description here"
-                }];
+<script src="https://unpkg.com/swiper/js/swiper.min.js"></script>
+ <script>
+                new Swiper('.swiper-container', {
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    });
             </script>
-        </div>
-    </div>
-    <!--/header-->
-</body>
-</html>
